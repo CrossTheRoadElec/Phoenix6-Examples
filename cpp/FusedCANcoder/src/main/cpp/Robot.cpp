69dd1d304fed19ad65c7226a5745ac32be7c30c9
CoryNessCTR <cness@ctr-electronics.com>
2023-01-27 17:11:05 -0500

    Update versions for 23.0.5 & add some new examples

---
+++ cpp/FusedCANcoder/src/main/cpp/Robot.cpp
@@ -0,0 +1,90 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "Robot.h"
+#include <iostream>
+
+using namespace ctre::phoenixpro;
+
+void Robot::RobotInit() {
+  /* Configure CANcoder to zero the magnet appropriately */
+  configs::CANcoderConfiguration cc_cfg{};
+  cc_cfg.MagnetSensor.AbsoluteSensorRange = signals::AbsoluteSensorRangeValue::Signed_PlusMinusHalf;
+  cc_cfg.MagnetSensor.SensorDirection = signals::SensorDirectionValue::CounterClockwise_Positive;
+  cc_cfg.MagnetSensor.MagnetOffset = 0.4;
+  m_cc.GetConfigurator().Apply(cc_cfg);
+
+  configs::TalonFXConfiguration fx_cfg{};
+  fx_cfg.Feedback.FeedbackRemoteSensorID = m_cc.GetDeviceID();
+  fx_cfg.Feedback.FeedbackSensorSource = signals::FeedbackSensorSourceValue::FusedCANcoder;
+  fx_cfg.Feedback.SensorToMechanismRatio = 1.0;
+  fx_cfg.Feedback.RotorToSensorRatio = 12.8;
+
+  m_fx.GetConfigurator().Apply(fx_cfg);
+}
+void Robot::RobotPeriodic() {
+  if (printCount++ > 10) {
+    printCount = 0;
+    // If any faults happen, print them out. Sticky faults will always be present if live-fault occurs
+    f_fusedSensorOutOfSync.Refresh();
+    sf_fusedSensorOutOfSync.Refresh();
+    f_missingRemoteSensor.Refresh();
+    sf_missingRemoteSensor.Refresh();
+    bool anyFault = sf_fusedSensorOutOfSync.GetValue() || sf_missingRemoteSensor.GetValue();
+    if(anyFault) {
+      std::cout << "A fault has occurred:" << std::endl;;
+      /* If we're live, indicate live, otherwise if we're sticky indicate sticky, otherwise do nothing */
+      if(f_fusedSensorOutOfSync.GetValue()) {
+        std::cout << "Fused sensor out of sync live-faulted" << std::endl;
+      } else if (sf_fusedSensorOutOfSync.GetValue()) {
+        std::cout << "Fused sensor out of sync sticky-faulted" << std::endl;
+      }
+      /* If we're live, indicate live, otherwise if we're sticky indicate sticky, otherwise do nothing */
+      if(f_missingRemoteSensor.GetValue()) {
+        std::cout << "Missing remote sensor live-faulted" << std::endl;
+      } else if (sf_missingRemoteSensor.GetValue()) {
+        std::cout << "Missing remote sensor sticky-faulted" << std::endl;
+      }
+    }
+
+    if(m_joystick.GetAButton()) {
+      /* Clear sticky faults */
+      m_fx.ClearStickyFaults();
+    }
+
+    /* Print out current position and velocity */
+    fx_pos.Refresh(); fx_vel.Refresh();
+    cc_pos.Refresh(); cc_vel.Refresh();
+    std::cout << "FX Position: " << fx_pos << " FX Vel: " << fx_vel << std::endl;
+    std::cout << "CC Position: " << cc_pos << " CC Vel: " << cc_vel << std::endl;
+    std::cout << "" << std::endl;
+  }
+}
+
+void Robot::AutonomousInit() {}
+void Robot::AutonomousPeriodic() {}
+
+void Robot::TeleopInit() {}
+void Robot::TeleopPeriodic() {
+  double output = m_joystick.GetLeftY();
+  if (output > -0.1 && output < 0.1) output = 0;
+  if(!m_joystick.GetBButton())
+    output *= 0.1;
+  m_fx.SetControl(m_dutyCycleControl.WithOutput(output));
+}
+
+void Robot::DisabledInit() {}
+void Robot::DisabledPeriodic() {}
+
+void Robot::TestInit() {}
+void Robot::TestPeriodic() {}
+
+void Robot::SimulationInit() {}
+void Robot::SimulationPeriodic() {}
+
+#ifndef RUNNING_FRC_TESTS
+int main() {
+  return frc::StartRobot<Robot>();
+}
+#endif
---
+++ java/MotionMagic/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,115 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.ctre.phoenixpro.StatusCode;
+import com.ctre.phoenixpro.configs.FeedbackConfigs;
+import com.ctre.phoenixpro.configs.MotionMagicConfigs;
+import com.ctre.phoenixpro.configs.Slot0Configs;
+import com.ctre.phoenixpro.configs.TalonFXConfiguration;
+import com.ctre.phoenixpro.controls.MotionMagicVoltage;
+import com.ctre.phoenixpro.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  TalonFX m_motor = new TalonFX(1, "fred");
+  MotionMagicVoltage m_mmReq = new MotionMagicVoltage(0);
+  XboxController m_joystick = new XboxController(0);
+  int m_printCount = 0;
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  @Override
+  public void robotInit() {
+    TalonFXConfiguration cfg = new TalonFXConfiguration();
+
+    /* Configure current limits */
+    MotionMagicConfigs mm = new MotionMagicConfigs();
+    mm.MotionMagicCruiseVelocity = 5; // 5 rotations per second cruise
+    mm.MotionMagicAcceleration = 10; // Take approximately 0.5 seconds to reach max vel
+    // Take approximately 0.2 seconds to reach max accel 
+    mm.MotionMagicJerk = 50;
+    cfg.MotionMagic = mm;
+
+    Slot0Configs slot0 = new Slot0Configs();
+    slot0.kP = 60;
+    slot0.kI = 0;
+    slot0.kD = 0.1;
+    slot0.kV = 0.12;
+    slot0.kS = 0.25; // Approximately 0.25V to get the mechanism moving
+    cfg.Slot0 = slot0;
+
+    FeedbackConfigs fdb = new FeedbackConfigs();
+    fdb.SensorToMechanismRatio = 12.8;
+    cfg.Feedback = fdb;
+
+    StatusCode status = StatusCode.StatusCodeNotInitialized;
+    for(int i = 0; i < 5; ++i) {
+      status = m_motor.getConfigurator().apply(cfg);
+      if (status.isOK()) break;
+    }
+    if (!status.isOK()) {
+      System.out.println("Could not configure device. Error: " + status.toString());
+    }
+  }
+
+  @Override
+  public void robotPeriodic() {
+    if (m_printCount++ > 10) {
+      m_printCount = 0;
+      System.out.println("Pos: " + m_motor.getPosition());
+      System.out.println("Vel: " + m_motor.getVelocity());
+      System.out.println();
+    }
+  }
+
+  @Override
+  public void autonomousInit() {}
+
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {}
+
+  @Override
+  public void teleopPeriodic() {
+    /* Deadband the joystick */
+    double leftY = m_joystick.getLeftY();
+    if(leftY > -0.1 && leftY < 0.1) leftY = 0;
+
+    m_motor.setControl(m_mmReq.withPosition(leftY * 10).withSlot(0));
+    if(m_joystick.getBButton()) {
+      m_motor.setRotorPosition(1);
+    }
+  }
+
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void testInit() {}
+
+  @Override
+  public void testPeriodic() {}
+
+  @Override
+  public void simulationInit() {}
+
+  @Override
+  public void simulationPeriodic() {}
+}
