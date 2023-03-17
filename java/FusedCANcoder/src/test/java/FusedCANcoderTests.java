4410a359caf6b851770c4f39dfa90fa5fad302ba
CoryNessCTR <cness@ctr-electronics.com>
2023-03-17 14:17:15 -0400

    Update test to include Remote CANcoder as well as Fused CANcoder

--- java/FusedCANcoder/src/test/java/FusedCANcoderTests.java
+++ java/FusedCANcoder/src/test/java/FusedCANcoderTests.java
@@ -106,4 +106,48 @@ public class FusedCANcoderTests {
         assertEquals(talonPos.getValue(), CANCODER_POSITION, SET_DELTA);
         assertEquals(cancoderPos.getValue(), CANCODER_POSITION, SET_DELTA);
     }
+
+    @Test
+    public void testRemoteCANcoderPos() {
+        final double TALON_POSITION = 0.4;
+        final double CANCODER_POSITION = -3.1;
+
+        /* Configure Talon to use Fused CANcoder, factory default CANcoder */
+        var configs = new TalonFXConfiguration();
+        configs.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RemoteCANcoder;
+        configs.Feedback.FeedbackRemoteSensorID = cancoder.getDeviceID();
+        talon.getConfigurator().apply(configs);
+        cancoder.getConfigurator().apply(new CANcoderConfiguration());
+
+        /* Get sim states */
+        var talonSimState = talon.getSimState();
+        var cancoderSimState = cancoder.getSimState();
+
+        /* Wait for signal to update and assert they match the set positions */
+        var talonPos = talon.getPosition();
+        var cancoderPos = cancoder.getPosition();
+
+        /* Make sure both are initially set to 0 before messing with sim state */
+        talonSimState.setRawRotorPosition(0);
+        cancoderSimState.setRawPosition(0);
+        talon.setRotorPosition(0);
+        cancoder.setPosition(0);
+        /* Wait for sets to take affect */
+        BaseStatusSignalValue.waitForAll(1.0, talonPos, cancoderPos);
+
+        /* Set them to different values */
+        talonSimState.setRawRotorPosition(TALON_POSITION);
+        cancoderSimState.setRawPosition(CANCODER_POSITION);
+        
+        BaseStatusSignalValue.waitForAll(1.0, talonPos, cancoderPos);
+
+        /* Further wait for Talon, since it probably just received the new CANcoder position frame */
+        talonPos.waitForUpdate(1.0);
+
+        /* Make sure Talon matches CANcoder, since it should be using CANcoder's position */
+        System.out.println("Talon Pos vs expected: " + talonPos + " vs " + CANCODER_POSITION);
+        System.out.println("CANcoder Pos vs expected: " + cancoderPos + " vs " + CANCODER_POSITION);
+        assertEquals(talonPos.getValue(), CANCODER_POSITION, SET_DELTA);
+        assertEquals(cancoderPos.getValue(), CANCODER_POSITION, SET_DELTA);
+    }
 }
