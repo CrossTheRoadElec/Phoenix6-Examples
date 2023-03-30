1dda9f9ada8ee4a6f50025b1015f3233add1b80a
CoryNessCTR <cness@ctr-electronics.com>
2023-03-30 11:04:28 -0400

    Added more unit tests to the Pigeon2 example

--- java/Pigeon2/src/test/java/Pigeon2Tests.java
+++ java/Pigeon2/src/test/java/Pigeon2Tests.java
@@ -2,6 +2,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.ctre.phoenixpro.hardware.Pigeon2;
 import edu.wpi.first.hal.HAL;
+import edu.wpi.first.wpilibj.Timer;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -25,9 +26,63 @@ public class Pigeon2Tests {
         var yawGetter = pidgey.getYaw();
         var cfg = pidgey.getConfigurator();
 
+        /* First make simulation yaw 0 */
+        var simState= pidgey.getSimState();
+        simState.setRawYaw(0);
+
         cfg.setYaw(firstSet);
-        assertEquals(yawGetter.waitForUpdate(1).getValue(), firstSet, SET_DELTA);
+        yawGetter.waitForUpdate(1);
+        System.out.println("First yaw is " + firstSet + " vs Pidgey's " + yawGetter.getValue());
+        assertEquals(yawGetter.getValue(), firstSet, SET_DELTA);
         cfg.setYaw(secondSet);
-        assertEquals(yawGetter.waitForUpdate(1).getValue(), secondSet, SET_DELTA);
+        yawGetter.waitForUpdate(1);
+        System.out.println("Second yaw is " + secondSet + " vs Pidgey's " + yawGetter.getValue());
+        assertEquals(yawGetter.getValue(), secondSet, SET_DELTA);
+    }
+
+    @Test
+    public void testSimulationYawSetter() {
+        final double firstSet = 0;
+        final double secondSet = 24;
+        var yawGetter = pidgey.getYaw();
+        var cfg = pidgey.getConfigurator();
+
+        /* First make simulation yaw 0 */
+        var simState= pidgey.getSimState();
+        simState.setRawYaw(0);
+        cfg.setYaw(0);
+
+        simState.setRawYaw(firstSet);
+        yawGetter.waitForUpdate(1);
+        System.out.println("First yaw is " + firstSet + " vs Pidgey's " + yawGetter.getValue());
+        assertEquals(yawGetter.getValue(), firstSet, SET_DELTA);
+        simState.setRawYaw(secondSet);
+        yawGetter.waitForUpdate(1);
+        System.out.println("Second yaw is " + secondSet + " vs Pidgey's " + yawGetter.getValue());
+        assertEquals(yawGetter.getValue(), secondSet, SET_DELTA);
+    }
+
+    @Test
+    public void testSimulationRotation2d() {
+        final double firstSet = 0;
+        final double secondSet = 24;
+        var cfg = pidgey.getConfigurator();
+
+        /* First make simulation yaw 0 */
+        var simState= pidgey.getSimState();
+        simState.setRawYaw(0);
+        cfg.setYaw(0);
+
+        simState.setRawYaw(firstSet);
+        Timer.delay(0.1); // There is no synchronous getRotation2d, so we instead have to delay
+        var rotation = pidgey.getRotation2d();
+        System.out.println("First yaw is " + firstSet + " vs rotation 2d's " + rotation.getDegrees());
+        assertEquals(rotation.getDegrees(), firstSet, SET_DELTA);
+
+        simState.setRawYaw(secondSet);
+        Timer.delay(0.1); // There is no synchronous getRotation2d, so we instead have to delay
+        rotation = pidgey.getRotation2d();
+        System.out.println("Second yaw is " + secondSet + " vs rotation 2d's " + rotation.getDegrees());
+        assertEquals(rotation.getDegrees(), secondSet, SET_DELTA);
     }
 }
