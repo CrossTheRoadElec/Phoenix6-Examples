b32901276ffa06fcbb60781f3a14834dcf4f4a16
CoryNessCTR <cness@ctr-electronics.com>
2023-12-01 14:08:23 -0500

    Add control request limits & update to beta-4

---
+++ java/ControlRequestLimits/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,98 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.ForwardLimitSourceValue;
+import com.ctre.phoenix6.signals.ForwardLimitTypeValue;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.ReverseLimitSourceValue;
+import com.ctre.phoenix6.signals.ReverseLimitTypeValue;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  private final TalonFX m_fx = new TalonFX(0, "*"); 
+
+  private final DutyCycleOut m_out = new DutyCycleOut(0);
+
+  private final XboxController m_joystick = new XboxController(0);
+
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  @Override
+  public void robotInit() {
+    TalonFXConfiguration configs = new TalonFXConfiguration();
+
+    configs.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+    configs.HardwareLimitSwitch.ForwardLimitSource = ForwardLimitSourceValue.RemoteCANcoder;
+    configs.HardwareLimitSwitch.ForwardLimitType = ForwardLimitTypeValue.NormallyOpen;
+    configs.HardwareLimitSwitch.ForwardLimitRemoteSensorID = 1;
+    configs.HardwareLimitSwitch.ForwardLimitEnable = true;
+    /* Retry config apply up to 5 times, report if failure */
+    StatusCode status = StatusCode.StatusCodeNotInitialized;
+    for (int i = 0; i < 5; ++i) {
+      status = m_fx.getConfigurator().apply(configs);
+      if (status.isOK()) break;
+    }
+    if(!status.isOK()) {
+      System.out.println("Could not apply configs, error code: " + status.toString());
+    }
+  }
+
+  @Override
+  public void robotPeriodic() {
+  }
+
+  @Override
+  public void autonomousInit() {}
+
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {}
+
+  @Override
+  public void teleopPeriodic() {
+    m_fx.setControl(m_out.withOutput(m_joystick.getLeftY())
+                         .withLimitForwardMotion(m_joystick.getLeftBumper())
+                         .withLimitReverseMotion(m_joystick.getRightBumper()));
+  }
+
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void testInit() {}
+
+  @Override
+  public void testPeriodic() {}
+
+  @Override
+  public void simulationInit() {}
+
+  @Override
+  public void simulationPeriodic() {
+    var simstate = m_fx.getSimState();
+    simstate.setForwardLimit(m_joystick.getAButton());
+    simstate.setReverseLimit(m_joystick.getBButton());
+  }
+}
---
+++ java/ControlRequestLimits/src/test/java/LimitTests.java
@@ -0,0 +1,343 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.*;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.hal.HAL;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.simulation.DriverStationSim;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class LimitTests {
+    final double SET_DELTA = 0.01;
+    final int CONFIG_RETRY_COUNT = 5;
+
+    TalonFX talonfx;
+
+    private enum DriveDirection
+    {
+        Zero,
+        Forward,
+        Reverse
+    }
+
+    private class ControlRequestTest<T extends ControlRequest>
+    {
+        public final T request;
+        public Consumer<DriveDirection> direction = null;
+        public Consumer<Boolean> limitForward, limitReverse = null;
+        public ControlRequestTest(T request)
+        {
+            this.request = request;
+        }
+    }
+
+    ControlRequestTest<?>[] tests = new ControlRequestTest<?>[]
+    {
+        new ControlRequestTest<DutyCycleOut>(new DutyCycleOut(0)) {{
+            direction = dir -> {
+                if (dir == DriveDirection.Zero) request.Output = 0;
+                if (dir == DriveDirection.Forward) request.Output = 1;
+                if (dir == DriveDirection.Reverse) request.Output = -1;
+            };
+            limitForward = request::withLimitForwardMotion;
+            limitReverse = request::withLimitReverseMotion;
+        }},
+        new ControlRequestTest<PositionDutyCycle>(new PositionDutyCycle(0)) {{
+            direction = dir -> {
+                if (dir == DriveDirection.Zero) request.Position = 0;
+                if (dir == DriveDirection.Forward) request.Position = 1;
+                if (dir == DriveDirection.Reverse) request.Position = -1;
+            };
+            limitForward = request::withLimitForwardMotion;
+            limitReverse = request::withLimitReverseMotion;
+        }},
+        new ControlRequestTest<VelocityDutyCycle>(new VelocityDutyCycle(0)) {{
+            direction = dir -> {
+                if (dir == DriveDirection.Zero) request.Velocity = 0;
+                if (dir == DriveDirection.Forward) request.Velocity = 1;
+                if (dir == DriveDirection.Reverse) request.Velocity = -1;
+            };
+            limitForward = request::withLimitForwardMotion;
+            limitReverse = request::withLimitReverseMotion;
+        }},
+        new ControlRequestTest<MotionMagicDutyCycle>(new MotionMagicDutyCycle(0)) {{
+            direction = dir -> {
+                if (dir == DriveDirection.Zero) request.Position = 0;
+                if (dir == DriveDirection.Forward) request.Position = 1;
+                if (dir == DriveDirection.Reverse) request.Position = -1;
+            };
+            limitForward = request::withLimitForwardMotion;
+            limitReverse = request::withLimitReverseMotion;
+        }},
+        new ControlRequestTest<DynamicMotionMagicDutyCycle>(new DynamicMotionMagicDutyCycle(0, 0, 0, 0)) {{
+            direction = dir -> {
+                if (dir == DriveDirection.Zero) {
+                    request.Position = 0;
+                    request.Velocity = 10000;
+                    request.Acceleration = 10000;
+                    request.Jerk = 0;
+                }
+                if (dir == DriveDirection.Forward) {
+                    request.Position = 1;
+                    request.Velocity = 10000;
+                    request.Acceleration = 10000;
+                    request.Jerk = 0;
+                }
+                if (dir == DriveDirection.Reverse) {
+                    request.Position = -1;
+                    request.Velocity = 10000;
+                    request.Acceleration = 10000;
+                    request.Jerk = 0;
+                }
+            };
+            limitForward = request::withLimitForwardMotion;
+            limitReverse = request::withLimitReverseMotion;
+        }},
+        new ControlRequestTest<VoltageOut>(new VoltageOut(0)) {{
+            direction = dir -> {
+                if (dir == DriveDirection.Zero) request.Output = 0;
+                if (dir == DriveDirection.Forward) request.Output = 1;
+                if (dir == DriveDirection.Reverse) request.Output = -1;
+            };
+            limitForward = request::withLimitForwardMotion;
+            limitReverse = request::withLimitReverseMotion;
+        }},
+        new ControlRequestTest<PositionVoltage>(new PositionVoltage(0)) {{
+            direction = dir -> {
+                if (dir == DriveDirection.Zero) request.Position = 0;
+                if (dir == DriveDirection.Forward) request.Position = 1;
+                if (dir == DriveDirection.Reverse) request.Position = -1;
+            };
+            limitForward = request::withLimitForwardMotion;
+            limitReverse = request::withLimitReverseMotion;
+        }},
+        new ControlRequestTest<VelocityVoltage>(new VelocityVoltage(0)) {{
+            direction = dir -> {
+                if (dir == DriveDirection.Zero) request.Velocity = 0;
+                if (dir == DriveDirection.Forward) request.Velocity = 1;
+                if (dir == DriveDirection.Reverse) request.Velocity = -1;
+            };
+            limitForward = request::withLimitForwardMotion;
+            limitReverse = request::withLimitReverseMotion;
+        }},
+        new ControlRequestTest<MotionMagicVoltage>(new MotionMagicVoltage(0)) {{
+            direction = dir -> {
+                if (dir == DriveDirection.Zero) request.Position = 0;
+                if (dir == DriveDirection.Forward) request.Position = 1;
+                if (dir == DriveDirection.Reverse) request.Position = -1;
+            };
+            limitForward = request::withLimitForwardMotion;
+            limitReverse = request::withLimitReverseMotion;
+        }},
+        new ControlRequestTest<DynamicMotionMagicVoltage>(new DynamicMotionMagicVoltage(0, 0, 0, 0)) {{
+            direction = dir -> {
+                if (dir == DriveDirection.Zero) {
+                    request.Position = 0;
+                    request.Velocity = 10000;
+                    request.Acceleration = 10000;
+                    request.Jerk = 0;
+                }
+                if (dir == DriveDirection.Forward) {
+                    request.Position = 1;
+                    request.Velocity = 10000;
+                    request.Acceleration = 10000;
+                    request.Jerk = 0;
+                }
+                if (dir == DriveDirection.Reverse) {
+                    request.Position = -1;
+                    request.Velocity = 10000;
+                    request.Acceleration = 10000;
+                    request.Jerk = 0;
+                }
+            };
+            limitForward = request::withLimitForwardMotion;
+            limitReverse = request::withLimitReverseMotion;
+        }},
+
+        /* TorqueCurrentFOC is not yet supported in sim, so don't unit test them */
+        // new ControlRequestTest<TorqueCurrentFOC>(new TorqueCurrentFOC(0)) {{
+        //     direction = dir -> {
+        //         if (dir == DriveDirection.Zero) request.Output = 0;
+        //         if (dir == DriveDirection.Forward) request.Output = 1;
+        //         if (dir == DriveDirection.Reverse) request.Output = -1;
+        //     };
+        //     limitForward = request::withLimitForwardMotion;
+        //     limitReverse = request::withLimitReverseMotion;
+        // }},
+        // new ControlRequestTest<PositionTorqueCurrentFOC>(new PositionTorqueCurrentFOC(0)) {{
+        //     direction = dir -> {
+        //         if (dir == DriveDirection.Zero) request.Position = 0;
+        //         if (dir == DriveDirection.Forward) request.Position = 1;
+        //         if (dir == DriveDirection.Reverse) request.Position = -1;
+        //     };
+        //     limitForward = request::withLimitForwardMotion;
+        //     limitReverse = request::withLimitReverseMotion;
+        // }},
+        // new ControlRequestTest<VelocityTorqueCurrentFOC>(new VelocityTorqueCurrentFOC(0)) {{
+        //     direction = dir -> {
+        //         if (dir == DriveDirection.Zero) request.Velocity = 0;
+        //         if (dir == DriveDirection.Forward) request.Velocity = 1;
+        //         if (dir == DriveDirection.Reverse) request.Velocity = -1;
+        //     };
+        //     limitForward = request::withLimitForwardMotion;
+        //     limitReverse = request::withLimitReverseMotion;
+        // }},
+        // new ControlRequestTest<MotionMagicTorqueCurrentFOC>(new MotionMagicTorqueCurrentFOC(0)) {{
+        //     direction = dir -> {
+        //         if (dir == DriveDirection.Zero) request.Position = 0;
+        //         if (dir == DriveDirection.Forward) request.Position = 1;
+        //         if (dir == DriveDirection.Reverse) request.Position = -1;
+        //     };
+        //     limitForward = request::withLimitForwardMotion;
+        //     limitReverse = request::withLimitReverseMotion;
+        // }},
+        // new ControlRequestTest<DynamicMotionMagicTorqueCurrentFOC>(new DynamicMotionMagicTorqueCurrentFOC(0, 0, 0, 0)) {{
+        //     direction = dir -> {
+        //         if (dir == DriveDirection.Zero) {
+        //             request.Position = 0;
+        //             request.Velocity = 10000;
+        //             request.Acceleration = 10000;
+        //             request.Jerk = 0;
+        //         }
+        //         if (dir == DriveDirection.Forward) {
+        //             request.Position = 1;
+        //             request.Velocity = 10000;
+        //             request.Acceleration = 10000;
+        //             request.Jerk = 0;
+        //         }
+        //         if (dir == DriveDirection.Reverse) {
+        //             request.Position = -1;
+        //             request.Velocity = 10000;
+        //             request.Acceleration = 10000;
+        //             request.Jerk = 0;
+        //         }
+        //     };
+        //     limitForward = request::withLimitForwardMotion;
+        //     limitReverse = request::withLimitReverseMotion;
+        // }},
+    };
+
+    @BeforeEach
+    public void constructDevices() {
+        assert HAL.initialize(500, 0);
+
+        talonfx = new TalonFX(0);
+
+        TalonFXConfiguration cfg = new TalonFXConfiguration();
+        cfg.Slot0.kP = 1; // Have some kP in slot 0 so we can drive with forward/reverse pos/vel
+        cfg.MotionMagic.MotionMagicAcceleration = 10000; // Very large number to get a change in output quickly
+        cfg.MotionMagic.MotionMagicCruiseVelocity = 10000; // Very large number to get a change in output quickly
+        retryConfigApply(()->talonfx.getConfigurator().apply(cfg));
+        retryConfigApply(()->talonfx.setPosition(0));
+
+        DriverStationSim.setEnabled(true);
+        DriverStationSim.notifyNewData();
+
+        Timer.delay(0.1);
+    }
+
+    @Test
+    public void robotIsEnabled()
+    {
+        assertTrue(DriverStation.isEnabled());
+    }
+
+    @Test
+    public void testAllControlLimits() {
+        for (var test : tests)
+        {
+            testControl(test);
+        }
+    }
+
+    private void retryConfigApply(Supplier<StatusCode> toApply) {
+        StatusCode finalCode = StatusCode.StatusCodeNotInitialized;
+        int triesLeftOver = CONFIG_RETRY_COUNT;
+        do {
+            finalCode = toApply.get();
+        } while (!finalCode.isOK() && --triesLeftOver > 0);
+        assert(finalCode.isOK());
+    }
+
+    private <T extends ControlRequest> void testControl(ControlRequestTest<T> test)
+    {
+        for(var dir : DriveDirection.values())
+        {
+            testControlPriv(test, dir, false, false);
+            testControlPriv(test, dir, false, true);
+            testControlPriv(test, dir, true, false);
+            testControlPriv(test, dir, true, true);
+        }
+    }
+    private <T extends ControlRequest> void testControlPriv(ControlRequestTest<T> test, DriveDirection direction, boolean forwardLimit, boolean reverseLimit)
+    {
+        var appliedMotorVoltage = talonfx.getMotorVoltage();
+        var forwardLimitStatus = talonfx.getFault_ForwardHardLimit();
+        var reverseLimitStatus = talonfx.getFault_ReverseHardLimit();
+
+        test.limitForward.accept(forwardLimit);
+        test.limitReverse.accept(reverseLimit);
+        test.direction.accept(direction);
+
+        talonfx.setControl(test.request);
+
+        for(int i = 0; i < 10; ++i)
+        {
+            talonfx.getSimState().setRawRotorPosition(0); // Set position/velocity so we can drive in the desired direction
+            talonfx.getSimState().setRotorVelocity(0);
+            Timer.delay(0.01); /* Delay just enough for things like motion magic to ramp */
+        }
+        BaseStatusSignal.waitForAll(1, appliedMotorVoltage, forwardLimitStatus, reverseLimitStatus);
+
+        System.out.println("");
+        System.out.println("Testing Control request " + test.request.getName());
+        System.out.println("Testing Direction " + direction.toString());
+        System.out.println("Applied out is " + appliedMotorVoltage);
+        System.out.println("Forward Limit is " + forwardLimit + " with fault " + forwardLimitStatus);
+        System.out.println("Reverse Limit is " + reverseLimit + " with fault " + reverseLimitStatus);
+        switch(direction)
+        {
+            case Zero:
+                assertEquals(appliedMotorVoltage.getValue(), 0, SET_DELTA);
+                assertFalse(forwardLimitStatus.getValue());
+                assertFalse(reverseLimitStatus.getValue());
+                break;
+            case Forward:
+                if(forwardLimit)
+                {
+                    assertEquals(appliedMotorVoltage.getValue(), 0, SET_DELTA);
+                }
+                else
+                {
+                    assertTrue(appliedMotorVoltage.getValue() > SET_DELTA);
+                }
+                assertTrue(forwardLimitStatus.getValue() == forwardLimit);
+                assertFalse(reverseLimitStatus.getValue());
+                break;
+            case Reverse:
+                if(reverseLimit)
+                {
+                    assertEquals(appliedMotorVoltage.getValue(), 0, SET_DELTA);
+                }
+                else
+                {
+                    assertTrue(appliedMotorVoltage.getValue() < -SET_DELTA);
+                }
+                assertTrue(reverseLimitStatus.getValue() == reverseLimit);
+                assertFalse(forwardLimitStatus.getValue());
+                break;
+        }
+    }
+}
