9e9ba7af027fabe1485411dca377f5ef1150224c
Benjamin Hall <bhallctre@gmail.com>
2023-12-08 18:02:37 -0500

    Use references to the existing configs object

--- java/MotionMagic/src/main/java/frc/robot/Robot.java
+++ java/MotionMagic/src/main/java/frc/robot/Robot.java
@@ -49,24 +49,21 @@ public class Robot extends TimedRobot {
     TalonFXConfiguration cfg = new TalonFXConfiguration();
 
     /* Configure current limits */
-    MotionMagicConfigs mm = new MotionMagicConfigs();
+    MotionMagicConfigs mm = cfg.MotionMagic;
     mm.MotionMagicCruiseVelocity = 5; // 5 rotations per second cruise
     mm.MotionMagicAcceleration = 10; // Take approximately 0.5 seconds to reach max vel
     // Take approximately 0.2 seconds to reach max accel 
     mm.MotionMagicJerk = 50;
-    cfg.MotionMagic = mm;
 
-    Slot0Configs slot0 = new Slot0Configs();
+    Slot0Configs slot0 = cfg.Slot0;
     slot0.kP = 60;
     slot0.kI = 0;
     slot0.kD = 0.1;
     slot0.kV = 0.12;
     slot0.kS = 0.25; // Approximately 0.25V to get the mechanism moving
-    cfg.Slot0 = slot0;
 
-    FeedbackConfigs fdb = new FeedbackConfigs();
+    FeedbackConfigs fdb = cfg.Feedback;
     fdb.SensorToMechanismRatio = 12.8;
-    cfg.Feedback = fdb;
 
     StatusCode status = StatusCode.StatusCodeNotInitialized;
     for(int i = 0; i < 5; ++i) {
