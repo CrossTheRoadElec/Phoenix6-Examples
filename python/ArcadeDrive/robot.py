aab3e2fadc2b09275b037314f84cdc1cc423d400
CoryNessCTR <cness@ctr-electronics.com>
2023-12-12 13:31:17 -0500

    Rename ArcadeDrive & fix up

--- python/ArcadeDrive/robot.py
+++ python/ArcadeDrive/robot.py
@@ -3,29 +3,29 @@
     This is a demo program for tank drive in Python with Phoenix6
 """
 import wpilib
-from phoenix6 import CoreTalonFX, Follower, DutyCycleOut
-from wpilib.drive import DifferentialDrive
+from phoenix6 import TalonFX, Follower, DutyCycleOut
 
 class MyRobot(wpilib.TimedRobot):
     def robotInit(self):
         """Robot initialization function"""
 
         # Keep a reference to all the motor controllers used
-        self.front_left_motor = CoreTalonFX(0, "Fred")
-        self.rear_left_motor = CoreTalonFX(1, "Fred")
-        self.front_right_motor = CoreTalonFX(2, "Fred")
-        self.rear_right_motor = CoreTalonFX(3, "Fred")
+        canivore_name = "canivore"
+        self.front_left_motor = TalonFX(0, canivore_name)
+        self.rear_left_motor = TalonFX(1, canivore_name)
+        self.front_right_motor = TalonFX(2, canivore_name)
+        self.rear_right_motor = TalonFX(3, canivore_name)
 
         # Configure the rear motors to follow the front motors
-        follow_left_request = Follower(master_id=0, oppose_master_direction=False)
+        follow_left_request = Follower(0, False)
         self.rear_left_motor.set_control(follow_left_request)
 
-        follow_right_request = Follower(master_id=2, oppose_master_direction=False)
+        follow_right_request = Follower(2, False)
         self.rear_right_motor.set_control(follow_right_request)
 
         # Keep a reference to the DutyCycleOut control request to update periodically
-        self.left_out = DutyCycleOut(output=0, enable_foc=True, override_brake_dur_neutral=False)
-        self.right_out = DutyCycleOut(output=0, enable_foc=True, override_brake_dur_neutral=False)
+        self.left_out = DutyCycleOut(output=0)
+        self.right_out = DutyCycleOut(output=0)
 
         # Keep a reference to an Xbox Controller for teleop control
         self.joy = wpilib.XboxController(0)
@@ -36,13 +36,9 @@ class MyRobot(wpilib.TimedRobot):
         throttle = self.joy.getLeftY() * -1
         wheel = self.joy.getRightX() * 1
 
-        # Set DutyCycleOut outputs
-        self.left_out.output = throttle + wheel
-        self.right_out.output = throttle - wheel
-
         # And set the DutyCycleOut to the motor controllers
-        self.front_left_motor.set_control(self.left_out)
-        self.front_right_motor.set_control(self.right_out)
+        self.front_left_motor.set_control(self.left_out.with_output(throttle + wheel))
+        self.front_right_motor.set_control(self.right_out.with_output(throttle - wheel))
 
 
 if __name__ == "__main__":
