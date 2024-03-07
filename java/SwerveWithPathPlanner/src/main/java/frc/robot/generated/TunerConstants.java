8d21b125bd18159068301b82b373deeaaf555060
CoryNessCTR <cness@ctr-electronics.com>
2024-03-06 19:31:46 -0500

    Reduce slip current from 300 amps to 150

--- java/SwerveWithPathPlanner/src/main/java/frc/robot/generated/TunerConstants.java
+++ java/SwerveWithPathPlanner/src/main/java/frc/robot/generated/TunerConstants.java
@@ -35,7 +35,7 @@ public class TunerConstants {
 
     // The stator current at which the wheels start to slip;
     // This needs to be tuned to your individual robot
-    private static final double kSlipCurrentA = 300.0;
+    private static final double kSlipCurrentA = 150.0;
 
     // Theoretical free speed (m/s) at 12v applied output;
     // This needs to be tuned to your individual robot
