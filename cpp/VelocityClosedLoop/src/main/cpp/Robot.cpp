66a9922921015aaf740ab69af54093449d44d273
Benjamin Hall <bhallctre@gmail.com>
2024-05-29 12:09:55 -0400

    Clean up VelocityClosedLoop examples

--- cpp/VelocityClosedLoop/src/main/cpp/Robot.cpp
+++ cpp/VelocityClosedLoop/src/main/cpp/Robot.cpp
@@ -3,6 +3,7 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #include "Robot.h"
+#include <iostream>
 
 using namespace ctre::phoenix6;
 
@@ -10,32 +11,35 @@ void Robot::RobotInit() {
   configs::TalonFXConfiguration configs{};
 
   /* Voltage-based velocity requires a feed forward to account for the back-emf of the motor */
-  configs.Slot0.kP = 0.11; // An error of 1 rotation per second results in 2V output
-  configs.Slot0.kI = 0.5; // An error of 1 rotation per second increases output by 0.5V every second
-  configs.Slot0.kD = 0.0001; // A change of 1 rotation per second squared results in 0.0001 volts output
-  configs.Slot0.kV = 0.12; // Falcon 500 is a 500kV motor, 500rpm per V = 8.333 rps per V, 1/8.33 = 0.12 volts / Rotation per second
-
-  configs.Voltage.PeakForwardVoltage = 8;  // Peak output of 8 volts
-  configs.Voltage.PeakReverseVoltage = -8; // Peak output of 8 volts
+  configs.Slot0.kS = 0.1; // To account for friction, add 0.1 V of static feedforward
+  configs.Slot0.kV = 0.12; // Kraken X60 is a 500 kV motor, 500 rpm per V = 8.333 rps per V, 1/8.33 = 0.12 volts / rotation per second
+  configs.Slot0.kP = 0.11; // An error of 1 rotation per second results in 0.11 V output
+  configs.Slot0.kI = 0; // No output for integrated error
+  configs.Slot0.kD = 0; // No output for error derivative
+  // Peak output of 8 volts
+  configs.Voltage.PeakForwardVoltage = 8;
+  configs.Voltage.PeakReverseVoltage = -8;
   
   /* Torque-based velocity does not require a feed forward, as torque will accelerate the rotor up to the desired velocity by itself */
+  configs.Slot1.kS = 2.5; // To account for friction, add 2.5 A of static feedforward
   configs.Slot1.kP = 5; // An error of 1 rotation per second results in 5 amps output
-  configs.Slot1.kI = 0.1; // An error of 1 rotation per second increases output by 0.1 amps every second
-  configs.Slot1.kD = 0.001; // A change of 1000 rotation per second squared results in 1 amp output
+  configs.Slot1.kI = 0; // No output for integrated error
+  configs.Slot1.kD = 0; // No output for error derivative
+  // Peak output of 40 amps
+  configs.TorqueCurrent.PeakForwardTorqueCurrent = 40;
+  configs.TorqueCurrent.PeakReverseTorqueCurrent = -40;
 
-  configs.TorqueCurrent.PeakForwardTorqueCurrent = 40;  // Peak output of 40 amps
-  configs.TorqueCurrent.PeakReverseTorqueCurrent = -40; // Peak output of 40 amps
-  
-  /* Percent supply gains when we get a Slot 2 */
-  configs.Slot1.kP = 0.01; // An error of 100 rotations per second results in 100% output
-  configs.Slot1.kI = 0.04; // An error of 1 rotation per second increases output by 0.04V every second
-  configs.Slot1.kD = 0.00001; // A change of 1 rotation per second squared results in 0.00001 volts output
-  configs.Slot1.kV = 0.013; // Approximately 1.3% for each rotation per second
+  /* Retry config apply up to 5 times, report if failure */
+  ctre::phoenix::StatusCode status = ctre::phoenix::StatusCode::StatusCodeNotInitialized;
+  for (int i = 0; i < 5; ++i) {
+    status = m_fx.GetConfigurator().Apply(configs);
+    if (status.IsOK()) break;
+  }
+  if (!status.IsOK()) {
+    std::cout << "Could not apply configs, error code: " << status.GetName() << std::endl;
+  }
 
-  configs.MotorOutput.PeakForwardDutyCycle = 0.7;  // Peak output of 70%
-  configs.MotorOutput.PeakReverseDutyCycle = -0.7; // Peak output of 70%
-
-  m_fx.GetConfigurator().Apply(configs);
+  m_fllr.SetControl(controls::Follower{m_fx.GetDeviceID(), false});
 }
 void Robot::RobotPeriodic() {}
 
@@ -45,23 +49,17 @@ void Robot::AutonomousPeriodic() {}
 void Robot::TeleopInit() {}
 void Robot::TeleopPeriodic() {
   double joyValue = m_joystick.GetLeftY();
-  if (joyValue > -0.1 && joyValue < 0.1) joyValue = 0;
+  if (fabs(joyValue) < 0.1) joyValue = 0;
 
-  auto desiredRotationsPerSecond = joyValue * 50_tps; // Go for plus/minus 10 rotations per second
+  auto desiredRotationsPerSecond = joyValue * 50_tps; // Go for plus/minus 50 rotations per second
 
-  if (m_joystick.GetLeftBumper())
-  {
+  if (m_joystick.GetLeftBumper()) {
     /* Use voltage velocity */
     m_fx.SetControl(m_voltageVelocity.WithVelocity(desiredRotationsPerSecond));
-  }
-  else if (m_joystick.GetRightBumper())
-  {
-    auto friction_torque = (joyValue > 0) ? 1_A : -1_A; // To account for friction, we add this to the arbitrary feed forward
+  } else if (m_joystick.GetRightBumper()) {
     /* Use torque velocity */
-    m_fx.SetControl(m_torqueVelocity.WithVelocity(desiredRotationsPerSecond).WithFeedForward(friction_torque));
-  }
-  else
-  {
+    m_fx.SetControl(m_torqueVelocity.WithVelocity(desiredRotationsPerSecond));
+  } else {
     /* Disable the motor instead */
     m_fx.SetControl(m_brake);
   }
