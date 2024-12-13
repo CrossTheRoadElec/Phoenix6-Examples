a5558560eb3525bfb867d0c2c02f8311dfeff241
Benjamin Hall <bhallctre@gmail.com>
2024-12-12 20:04:43 -0500

    Add missing Java units usage in TunerConstants

--- java/SwerveWithPathPlanner/src/main/java/frc/robot/generated/TunerConstants.java
+++ java/SwerveWithPathPlanner/src/main/java/frc/robot/generated/TunerConstants.java
@@ -87,8 +87,8 @@ public class TunerConstants {
     private static final int kPigeonId = 1;
 
     // These are only used for simulation
-    private static final double kSteerInertia = 0.01;
-    private static final double kDriveInertia = 0.01;
+    private static final MomentOfInertia kSteerInertia = KilogramSquareMeters.of(0.01);
+    private static final MomentOfInertia kDriveInertia = KilogramSquareMeters.of(0.01);
     // Simulated voltage necessary to overcome friction
     private static final Voltage kSteerFrictionVoltage = Volts.of(0.2);
     private static final Voltage kDriveFrictionVoltage = Volts.of(0.2);
