f0ad66c70ee0405878326d29ec08f37ffec790fc
Dalton Smith <daltz@ctr-electronics.com>
2025-01-28 16:38:27 -0500

    Adjust verbiage

--- java/PWMTalonFXS/src/main/java/frc/robot/PWMTalonFXS.java
+++ java/PWMTalonFXS/src/main/java/frc/robot/PWMTalonFXS.java
@@ -1,3 +1,10 @@
+/*
+* Copyright (C) Cross The Road Electronics.  All rights reserved.
+* License information can be found in CTRE_LICENSE.txt
+* For support and suggestions contact support@ctr-electronics.com or file
+* an issue tracker at https://github.com/CrossTheRoadElec/Phoenix-Releases
+*/
+
 package frc.robot;
 
 import java.util.ArrayList;
