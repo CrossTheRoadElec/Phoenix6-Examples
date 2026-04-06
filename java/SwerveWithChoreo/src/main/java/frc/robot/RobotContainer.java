b526aff5ff6fac533e8cd0187c99aa7a7e6f7620
Benjamin Hall <bhall@ctr-electronics.com>
2026-04-06 12:19:30 -0400

    Update SwerveWithChoreo to include the new warmup command

--- java/SwerveWithChoreo/src/main/java/frc/robot/RobotContainer.java
+++ java/SwerveWithChoreo/src/main/java/frc/robot/RobotContainer.java
@@ -15,6 +15,7 @@ import choreo.auto.AutoFactory;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
@@ -54,6 +55,9 @@ public class RobotContainer {
         SmartDashboard.putData("Auto Chooser", autoChooser);
 
         configureBindings();
+
+        // Warmup Choreo to avoid Java pauses
+        CommandScheduler.getInstance().schedule(autoFactory.warmupCmd());
     }
 
     private void configureBindings() {
